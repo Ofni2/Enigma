7270e7f0c662e6e46f80850fa419655c35a012bd
ofni <guigui.spam@hotmail.fr>
2017-02-13 14:52:52 +0100

    UPD : Rotor modelisation

---
+++ Rotor.hpp
@@ -0,0 +1,50 @@
+#ifndef DEF_ROTOR
+#define DEF_ROTOR
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+// #ascii chiffré = [#ascii-97+decalage]%26+97
+
+class Rotor
+{
+public:
+    // Constructeur
+    Rotor();
+    Rotor(const char listPermutation[2][26],int ringSetting,int startPosition);
+
+    // Methodes
+    void get_permutationTab();
+	int get_ringSetting();
+	int get_rotorPosition();
+
+	void set_rotorPosition(int i);
+	void set_ringSetting(int i);
+
+    char activate(char charToPermute,int sens=1);
+    void rotate(int decalage=0);
+	int isValidChar(char c);
+
+
+private :
+    // Attributs
+    char rotor_tab[26][2];
+	int m_steppingPosition;
+	int m_ringSetting;
+	int m_rotorStartPosition;
+	char m_wiringTab[26][2];
+	char m_contactTab[26][2];
+	int m_position;
+
+
+    //Methodes
+    int m_seek(char x, char S);
+	int charToInt(char x);
+	char intToChar(int i);
+
+};
+
+#endif
+
+
